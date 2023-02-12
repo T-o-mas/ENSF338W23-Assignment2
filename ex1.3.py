84e91b7ea6394154cd48e7a598281a9fab4ad053
T-o-mas <Tomaskmet23@gmail.com>
2023-02-11 22:08:48 -0700

    fixed bugs for ex1.3.py

--- ex1.3.py
+++ ex1.3.py
@@ -4,10 +4,12 @@ Code using memoization to improve performance. Calculates the
 nth term in the fibonacci sequence. 
 """
 
-def func(n, memo=[]):
+def func2(n, memo={}):
     if n == 0 or n == 1:
         return n
     if n not in memo:
-        memo[n] = func(n-1, memo) + func(n-2, memo)
+        memo[n] = func2(n-1, memo) + func2(n-2, memo)
     return memo[n]
+
+
 
